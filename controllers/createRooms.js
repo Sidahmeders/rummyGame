bb7b07c4425dd6acefa1094dc1425689493c6207
Sidahmeders <ahorob57@gmail.com>
2021-10-31 10:50:20 +0100

    refactored the joinPlayers code

--- controllers/createRooms.js
+++ controllers/createRooms.js
@@ -2,27 +2,27 @@ const readJsonData = require('../utils/readJsonData')
 const writeJsonData = require('../utils/writeJsonData')
 
 module.exports = async function createRooms(req, res) {
-  const { roomName, password } = req.body
+    const { roomName, password } = req.body
 
-  if (!roomName || !password) {
-    res.status(400).json({ error: 'please fill in the userName and password' })
-    return
-  }
-
-  try {
-    let rooms = JSON.parse(readJsonData())
-
-    if (rooms[roomName]) {
-      throw Error('room name already exist..')
+    if (!roomName || !password) {
+        res.status(400).json({ error: 'please fill in the userName and password' })
+        return
     }
 
-    // handle pushing new rooms to database
-    rooms[roomName] = { password }
-    // write back the new data to our json file
-    await writeJsonData(rooms, 'new room has been added...')
+    try {
+        let rooms = JSON.parse(readJsonData())
 
-    res.status(201).json('new room added successfully')
-  } catch (err) {
-    res.status(400).json({ errorMsg: err.message })
-  }
+        if (rooms[roomName]) {
+            throw Error('room name already exist..')
+        }
+
+        // handle pushing new rooms to database
+        rooms[roomName] = { password, players: [] }
+        // write back the new data to our json file
+        await writeJsonData(rooms, 'new room has been added...')
+
+        res.status(201).json('new room added successfully')
+    } catch (err) {
+        res.status(400).json({ errorMsg: err.message })
+    }
 }
--- utils/joinPlayers.js
+++ utils/joinPlayers.js
@@ -1,7 +1,6 @@
 const readJsonData = require('./readJsonData')
 const writeJsonData = require('./writeJsonData')
 
-const roomsData = JSON.parse(readJsonData())
 let ioSocket
 
 module.exports = function joinPlayers({ socket, roomInfo }) {
@@ -17,57 +16,34 @@ module.exports = function joinPlayers({ socket, roomInfo }) {
 }
 
 async function handlePlayersJoiningRooms({ roomName, password, username }) {
-    let canWrite = false
+    const roomsData = JSON.parse(readJsonData())
+    const room = roomsData[roomName]
 
-    const rooms = roomsData.map((room) => {
-        const isValidPassword = validRoomAndPassword(room, roomName, password)
-        if (isValidPassword && validPlayers(room, username)) {
-            joinNewPlayers(room, username)
-            canWrite = true
-        }
-        return room
-    })
+    let roomPlayers = room.players,
+        roomPassword = room.password
 
-    if (canWrite) {
-        await writeJsonData(rooms, 'new player has been added...')
+    const isValidPassword = roomPassword === password
+    const isValidUsername = validUserName(roomPlayers, username)
+    const isValidRoom = roomPlayers.length < 4
+
+    if (!isValidRoom) {
+        ioSocket.emit('join-room-error', 'this room is already full')
+    } else if (!isValidUsername) {
+        ioSocket.emit('join-room-error', 'this username already exist')
+    } else if (!isValidPassword) {
+        ioSocket.emit('join-room-error', 'the given password is wrong')
+    } else {
+        roomPlayers.push(username)
+        await writeJsonData(roomsData, 'new player has been added...')
         ioSocket.emit('user-joined-room', roomName, username)
     }
 }
 
-function validRoomAndPassword(room, roomName, password) {
-    if (room.roomName === roomName && room.password === password) {
-        return true
-    }
-    ioSocket.emit('join-room-error', 'please make sure the password is correct')
-    return false
-}
-
-function validPlayers(room, username) {
-    roomPlayers = room['players'] || []
-    const userExist = doesUserExist(roomPlayers, username)
-    if (roomPlayers.length >= 4 || userExist) {
-        ioSocket.emit('join-room-error', 'this room is full OR username already exist')
-        return false
+function validUserName(roomPlayers, username) {
+    for (let player of roomPlayers) {
+        if (player == username) {
+            return false
+        }
     }
     return true
 }
-
-function doesUserExist(roomPlayers, username) {
-    for (let player of roomPlayers) {
-        if (player == username) {
-            return true
-        }
-    }
-    return false
-}
-
-function joinNewPlayers(room, username) {
-    if (!room['players']) {
-        room['players'] = [username]
-    } else {
-        room['players'].push(username)
-    }
-    return room
-}
-
-// TODO: REFACTOR THIS FUCKED UP CODE
