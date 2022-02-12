f399d364c13dc6f6eb17a59590484209a4b63615
Sidahmeders <ahorob57@gmail.com>
2022-02-13 00:30:04 +0100

    prevent favicon request and rendering of the peerPlayer Cards

--- events/index.js
+++ events/index.js
@@ -1,10 +1,10 @@
-const make_joinRoom = require('./joinRoom.js')
+const makeJoinRoom = require('./joinRoom.js')
 const makeGetRoomData = require('./getRoomData.js')
 const makeDragCards = require('./dragCards.js')
 const makeDropCards = require('./dropCards.js')
 const makeWebrtcSignaling = require('./webrtcSignaling.js')
 
-const joinRoom = (io, socket, payload) => make_joinRoom(io, socket, payload)
+const joinRoom = (io, socket, payload) => makeJoinRoom(io, socket, payload)
 const getRoomData = (io, socket, payload) => makeGetRoomData(io, socket, payload)
 const dragCards = (io, socket, payload) => makeDragCards(io, socket, payload)
 const dropCards = (io, socket, payload) => makeDropCards(io, socket, payload)
