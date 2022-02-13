34a00f1de068ff4edb4c374a2d362552bfd64878
Sidahmeders <ahorob57@gmail.com>
2022-02-13 13:56:18 +0100

    created and moved the connection and disconnection logic to the events folder

--- src/events/index.js
+++ src/events/index.js
@@ -1,4 +1,6 @@
 const events = require('../constant/events')
+const makeConnect = require('./connection')
+const makeDisConnect = require('./disconnect.js')
 const makeJoinRoom = require('./joinRoom.js')
 const makeGetRoomData = require('./getRoomData.js')
 const makeDragCards = require('./dragCards.js')
@@ -6,6 +8,9 @@ const makeDropCards = require('./dropCards.js')
 const makeWebrtcSignaling = require('./webrtcSignaling.js')
 
 module.exports = (io, socket) => {
+  const connection = () => makeConnect(socket)
+  const disconnect = () => makeDisConnect(socket)
+
   const joinRoom = (payload) => makeJoinRoom({ socket, payload, events })
   const getRoomData = (payload) => makeGetRoomData({ io, socket, payload, events })
   const dragCards = (payload) => makeDragCards({ socket, payload, events })
@@ -15,5 +20,5 @@ module.exports = (io, socket) => {
   const peerJoin = (room) => webrtcSignal.onPeerJoin(room)
   const peerMessage = (message) => webrtcSignal.onPeerMessage(message)
 
-  return { joinRoom, getRoomData, dragCards, dropCards, peerJoin, peerMessage }
+  return { connection, disconnect, joinRoom, getRoomData, dragCards, dropCards, peerJoin, peerMessage }
 }
--- src/listeners/index.js
+++ src/listeners/index.js
@@ -2,20 +2,18 @@ const listners = require('../constant/listeners')
 const eventHandlers = require('../events')
 
 const onConnection = (io, socket) => {
-  const { joinRoom, dragCards, dropCards, getRoomData, peerJoin, peerMessage } = eventHandlers(io, socket)
-
-  console.log(`user:: ${socket.id} ::connected`)
+  const { connection, disconnect, joinRoom, dragCards, dropCards, getRoomData, peerJoin, peerMessage } = eventHandlers(io, socket)
+  connection()
 
   socket.on(listners.joinRoom, joinRoom)
   socket.on(listners.roomsData, getRoomData)
-
   socket.on(listners.cardsDrag, dragCards)
   socket.on(listners.cardsDrop, dropCards)
 
   socket.on(listners.peersJoin, peerJoin)
   socket.on(listners.peersMessage, peerMessage)
 
-  socket.on(listners.disconnect, () => console.log(`user:: ${socket.id} ::disconnected`))
+  socket.on(listners.disconnect, disconnect)
 }
 
 module.exports = (io) => io.on('connection', (socket) => onConnection(io, socket))
