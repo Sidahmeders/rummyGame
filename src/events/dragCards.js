d20ad25d522d9c6656d53b2546b89c402a328910
Sidahmeders <ahorob57@gmail.com>
2022-02-13 14:18:36 +0100

    refactored the dropCards event module

--- src/events/dragCards.js
+++ src/events/dragCards.js
@@ -2,34 +2,23 @@ const getPlayerData = require('../utils/getPlayerData')
 const inMemoryActiveGames = require('../store/inMemoryGames')
 
 module.exports = ({ socket, payload, events }) => {
-  const { roomName } = payload
-
   try {
+    const { roomName, username } = payload
     const targetRoom = inMemoryActiveGames[roomName]
-    if (!targetRoom) {
-      socket.emit(events.roomsError, 'something unexpected happens. please refresh the page')
-    } else {
-      appendCard({ socket, payload, events, targetRoom })
-    }
-  } catch (err) {
-    socket.emit(events.roomsError, err.message)
-  }
-}
+    if (!targetRoom) throw Error('something unexpected happens. please refresh the page')
 
-function appendCard({ socket, payload, events, targetRoom }) {
-  const { username, roomName } = payload
-  const { cards, playersCards } = targetRoom
-  const pickedCard = cards.pop()
-  const playerHand = playersCards[username]
+    const { cards, playersCards } = targetRoom
 
-  if (playerHand.length >= 15) {
-    socket.emit(events.roomsError, 'please drop a card before you can pick again')
-  } else if (!pickedCard) {
-    socket.emit(events.roomsError, 'the cards deck is empty')
-  } else {
+    const pickedCard = cards.pop()
+    const playerHand = playersCards[username]
+
+    if (playerHand.length >= 15) throw Error('please drop a card before you can pick again')
+    if (!pickedCard) throw Error('the cards deck is empty')
+
     playerHand.push(pickedCard)
-
     const playerData = getPlayerData(username, roomName)
     socket.emit(events.cardsDragged, playerData)
+  } catch (err) {
+    socket.emit(events.roomsError, err.message)
   }
 }
