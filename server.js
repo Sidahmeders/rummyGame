03a754057e83bbca93ea75be88d98a04120b3074
Sidahmeders <ahorob57@gmail.com>
2022-02-13 02:34:07 +0100

    moved all the server app logic to src folder

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express')
-const app = express()
+const initListner = require('./src/listeners')
+const roomsRoutes = require('./src/routes/rooms.routes')
+const corsConfig = require('./src/middlewares/cors.config')
 
+const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
-
-const initListner = require('./listeners')
 initListner(io)
 
 app.use(express.static(__dirname + '/client'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-const corsConfig = require('./middlewares/cors.config')
 app.use((req, res, next) => corsConfig(req, res, next))
 
-app.use('/', require('./routes/rooms.routes'))
+app.use('/', roomsRoutes)
 
 const PORT = process.env.PORT || 5000
 server.listen(PORT, console.log(`server running on port ${PORT}..`))
--- src/routes/rooms.routes.js
+++ src/routes/rooms.routes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const path = require('path')
-const { createRooms, getAllRooms } = require('../controllers/index')
+const { createRooms, getAllRooms } = require('../controllers')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../client/index.html`))
+  res.sendFile(path.join(`${__dirname}/../../client/index.html`))
 })
 
 router.post('/create-rooms', (req, res) => {
@@ -17,7 +17,7 @@ router.get('/get-rooms', (req, res) => {
 })
 
 router.get('/room/:roomId', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../client/room.html`))
+  res.sendFile(path.join(`${__dirname}/../../client/room.html`))
 })
 
 module.exports = router
