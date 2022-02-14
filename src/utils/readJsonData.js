d8d27ae8d501463a4677413c1ca59b7208beccca
Sidahmeders <ahorob57@gmail.com>
2022-02-14 02:08:17 +0100

    changed the indentation of read and write json data

--- src/utils/readJsonData.js
+++ src/utils/readJsonData.js
@@ -2,13 +2,9 @@ const path = require('path')
 const fs = require('fs')
 
 module.exports = function readJsonData() {
-  const roomsData = fs.readFileSync(
-    path.join(`${__dirname}/../store`, 'rooms.json'),
-    'utf8',
-    (err, data) => {
-      if (err) throw err
-      return data
-    }
-  )
+  const roomsData = fs.readFileSync(path.join(`${__dirname}/../store`, 'rooms.json'), 'utf8', (err, data) => {
+    if (err) throw err
+    return data
+  })
   return roomsData
 }
--- src/utils/writeJsonData.js
+++ src/utils/writeJsonData.js
@@ -1,13 +1,9 @@
-const fs = require('fs')
 const path = require('path')
+const fs = require('fs')
 
 module.exports = function writeJsonData(data, message) {
-  fs.writeFileSync(
-    path.join(`${__dirname}/../store`, 'rooms.json'),
-    JSON.stringify(data),
-    (err) => {
-      if (err) throw Error(err.message)
-      console.log(message)
-    }
-  )
+  fs.writeFileSync(path.join(`${__dirname}/../store`, 'rooms.json'), JSON.stringify(data), (err) => {
+    if (err) throw Error(err.message)
+    console.log(message)
+  })
 }
