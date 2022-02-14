84f439df835106782bea17cdaa42f169a2dd30fd
Sidahmeders <ahorob57@gmail.com>
2022-02-14 16:13:11 +0100

    refactored the createRooms.js file

--- src/controllers/createRooms.js
+++ src/controllers/createRooms.js
@@ -2,23 +2,17 @@ const readJsonData = require('../utils/readJsonData')
 const writeJsonData = require('../utils/writeJsonData')
 
 module.exports = async function createRooms(req, res) {
-  const { roomName, password } = req.body
-  if (!roomName || !password) {
-    res.status(400).json({ errorMsg: 'please fill in the room name and password' })
-    return
-  }
-
   try {
+    const { roomName, password } = req.body
+    if (!roomName || !password) throw Error('please fill in the room name and password')
+
     let rooms = JSON.parse(readJsonData())
-    if (rooms[roomName]) {
-      throw Error('room name already exist..')
-    }
+    if (rooms[roomName]) throw Error('room name already exist..')
 
     // handle pushing new rooms to database
     rooms[roomName] = { password, players: [] }
     // write back the new data to our json file
     await writeJsonData(rooms, 'new room has been added...')
-
     res.status(201).json('new room added successfully')
   } catch (err) {
     res.status(400).json({ errorMsg: err.message })
