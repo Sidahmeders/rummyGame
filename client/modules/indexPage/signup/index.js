3eafde15ec2c929785481e145a98ed276fd47ec8
Sidahmeders <ahorob57@gmail.com>
2022-02-17 03:07:54 +0100

    added some basic styles to the signUp module

--- client/modules/indexPage/signup/index.js
+++ client/modules/indexPage/signup/index.js
@@ -1,6 +1,18 @@
+const formTextElements = ['username', 'email', 'password', 'age', 'country', 'city']
+
 export default function SignUp() {
   const signupContainer = document.getElementById('signup')
   const signupForm = document.createElement('form')
 
+  const h2El = document.createElement('h2')
+  h2El.innerText = 'SignUp'
+
+  formTextElements.forEach((elText) => {
+    const inputElement = document.createElement('input')
+    inputElement.placeholder = elText
+    signupForm.appendChild(inputElement)
+  })
+
+  signupContainer.appendChild(h2El)
   signupContainer.appendChild(signupForm)
 }
