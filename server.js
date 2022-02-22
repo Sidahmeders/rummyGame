4b261e2587bc138213280935ffc83355574dcf9a
Sidahmeders <ahorob57@gmail.com>
2022-02-22 21:28:19 +0100

    moved the socket.io comments to the wss/WebSocketAdapter

--- server.js
+++ server.js
@@ -20,23 +20,6 @@ const PORT = process.env.PORT || 5000
 server.listen(PORT, console.log(`server running on port ${PORT}..`))
 
 /**
- * @emits socket.emit('message',__"this_is_a_test") //sending to sender-client only
- * @emits socket.broadcast.emit('message',__"this_is_a_test") //sending to all clients except sender
- * @emits socket.broadcast.to('game').emit('message',__"nice_game") //sending to all clients in 'game' room(channel) except sender
- * @emits socket.to('game').emit('message',__"enjoy_the_game") //sending to sender client, only if they are in 'game' room(channel)
- * @emits socket.broadcast.to(socketid).emit('message',__"for_your_eyes_only') //sending to individual socketid
- * @emits io.emit('message',__"this_is_a_test") //sending to all clients, include sender
- * @emits io.in('game').emit('message',__"cool_game"); //sending to all clients in 'game' room(channel), include sender
- * @emits io.of('myNamespace').emit('message',__"gg") //sending to all clients in namespace 'myNamespace', include sender
- * @emits socket.emit() //send to all connected clients
- * @emits socket.broadcast.emit() //send to all connected clients except the one that sent the message
- * @emits socket.on() //event listener, can be called on client to execute on server
- * @emits io.sockets.socket() //for emiting to specific clients
- * @emits io.sockets.emit() //send to all connected clients (same as socket.emit)
- * @emits io.sockets.on() //initial connection from a client.
- */
-
-/**
  * valid suite (10-S, J-S, Q-S), (2-C, 2-H, 2-S), (4-S, 5-S, 6-S, ...)
  * valid suite (A-H, 2-H, 3-H, 4-H, 5-H, 6-H....)
  * 1 2 3 4.... J Q K A
--- src/infrastructure/wss/WebSocketAdapter.js
+++ src/infrastructure/wss/WebSocketAdapter.js
@@ -1,3 +1,20 @@
+/**
+ * @emits socket.emit('message',__"this_is_a_test") //sending to sender-client only
+ * @emits socket.broadcast.emit('message',__"this_is_a_test") //sending to all clients except sender
+ * @emits socket.broadcast.to('game').emit('message',__"nice_game") //sending to all clients in 'game' room(channel) except sender
+ * @emits socket.to('game').emit('message',__"enjoy_the_game") //sending to sender client, only if they are in 'game' room(channel)
+ * @emits socket.broadcast.to(socketid).emit('message',__"for_your_eyes_only') //sending to individual socketid
+ * @emits io.emit('message',__"this_is_a_test") //sending to all clients, include sender
+ * @emits io.in('game').emit('message',__"cool_game"); //sending to all clients in 'game' room(channel), include sender
+ * @emits io.of('myNamespace').emit('message',__"gg") //sending to all clients in namespace 'myNamespace', include sender
+ * @emits socket.emit() //send to all connected clients
+ * @emits socket.broadcast.emit() //send to all connected clients except the one that sent the message
+ * @emits socket.on() //event listener, can be called on client to execute on server
+ * @emits io.sockets.socket() //for emiting to specific clients
+ * @emits io.sockets.emit() //send to all connected clients (same as socket.emit)
+ * @emits io.sockets.on() //initial connection from a client.
+ */
+
 class WebSocketAdapter {
   constructor(ws, socket) {
     this.ws = ws
