d179452a12cb25c03f0c1ec5f882f4299ddaae29
Sidahmeders <ahorob57@gmail.com>
2022-02-23 16:15:26 +0100

    updated the valueObjects to return all the required props

--- src/domain/valueObjects/player.js
+++ src/domain/valueObjects/player.js
@@ -1,21 +1,13 @@
-module.exports = class Player {
-  #cards
-  #isOnline
-  #turnToPick
-
-  constructor({ cards = [], isOnline = false, turnToPick = false }) {
-    this.#cards = cards
-    this.#isOnline = isOnline
-    this.#turnToPick = turnToPick
-  }
-
-  get cards() {
-    return this.#cards
-  }
-  get isOnline() {
-    return this.#isOnline
-  }
-  get turnToPick() {
-    return this.#turnToPick
-  }
+module.exports = function Player({ cards = [], isOnline = false, turnToPick = false }) {
+  return Object.freeze({
+    get cards() {
+      return cards
+    },
+    get isOnline() {
+      return isOnline
+    },
+    get turnToPick() {
+      return turnToPick
+    },
+  })
 }
