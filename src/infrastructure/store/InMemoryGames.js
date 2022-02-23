f8919f7cf0e0462330f2998870174e3adddb3b7f
Sidahmeders <ahorob57@gmail.com>
2022-02-23 02:10:35 +0100

    added the InMemoroyGames method for set and retriveding data and changed the data structre of the players

--- src/infrastructure/store/InMemoryGames.js
+++ src/infrastructure/store/InMemoryGames.js
@@ -1,9 +1,52 @@
 module.exports = {
-  onlinePlayers: {},
-  testRoom: {
-    password: '1234',
-    cards: [],
-    playersCards: { yousef: [], amine: [], yassine: [] },
-    players: ['yousef', 'amine', 'yassine'],
+  roomsCollection: {
+    onlinePlayers: {},
+    testRoom: {
+      // cards: [],
+      // playersCards: { yousef: [], amine: [], yassine: [] },
+
+      password: '1234',
+      deckCards: [],
+      players: {
+        yousef: {
+          cards: [],
+          isOnline: false,
+          turnToPick: false,
+        },
+        amine: {
+          cards: [],
+          isOnline: false,
+          turnToPick: false,
+        },
+        yassine: {
+          cards: [],
+          isOnline: false,
+          turnToPick: false,
+        },
+      },
+    },
   },
+
+  getRoomData(roomName) {
+    const targetRoom = this.roomsCollection[roomName]
+    return targetRoom
+  },
+
+  setRoomData(roomName, newRoomData) {
+    let oldRoomData = new Object(this.roomsCollection[roomName])
+    this.roomsCollection[roomName] = Object.assign(oldRoomData, newRoomData)
+  },
+
+  joinPlayers(roomName, dbPlayers, deckCards) {
+    const targetRoom = this.roomsCollection[roomName]
+
+    dbPlayers.forEach((username) => {
+      const roomPlayers = targetRoom?.players
+      const playerCards = roomPlayers[username]?.cards
+
+      if (!playerCards) playerCards = deckCards.splice(0, 14)
+    })
+  },
+
+  createNewRoom({ roomName, dbPlayers, deckCards }) {},
 }
