a008a875aae63ec00450a519c5dc6c96ab0fb1ca
Sidahmeders <ahorob57@gmail.com>
2022-02-23 03:33:59 +0100

    imported the shared and value Objects dependency

--- src/domain/services/index.js
+++ src/domain/services/index.js
@@ -1,14 +1,15 @@
-const createDeck54 = require('./cards54')
-const { InMemoryGames } = require('../../infrastructure/store')
-const { roomsDB } = require('../../infrastructure/store')
+const { InMemoryGames, roomsDB } = require('../../infrastructure/store')
+const { Player } = require('../valueObjects')
+
+const createDeck54 = require('../shared/createDeck54')
 
 const makeGetPlayerRoomData = require('./getPlayerRoomData')
 const makeUpdateRoomData = require('./updateRoomData')
 const makeUpdateOnlinePlayers = require('./updateOnlinePlayers')
 const makeJoinRoom = require('./joinRoom')
 
-const getPlayerRoomData = makeGetPlayerRoomData({ InMemoryGames })
-const updateRoomData = makeUpdateRoomData({ roomsDB, InMemoryGames, createDeck54 })
+const getPlayerRoomData = makeGetPlayerRoomData({ InMemoryGames, Player })
+const updateRoomData = makeUpdateRoomData({ roomsDB, InMemoryGames, Player, createDeck54 })
 const joinRoom = makeJoinRoom({ roomsDB })
 const { addOnlinePlayers, removeOnlinePlayers } = makeUpdateOnlinePlayers({ InMemoryGames })
 
